4223c3b11569cc31e1b1fd37122647198b6ed44f
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2025-04-23 07:26:09 +0800

    Add constants

--- pages/testdiv.vue
+++ pages/testdiv.vue
@@ -5,34 +5,38 @@ const samo = ref(true)
 
 <template>
   <div 
-    class="relative w-full h-dvh bg-slate-900 overflow-hidden flex items-center justify-center"
+    class="relative w-full h-dvh bg-slate-900 overflow-hidden flex items-center justify-end"
   >
-    <div 
-      id="selector" 
-      ref="selector" 
-      class="absolute aspect-video w-full sm:w-3/4 z-10"
+    <main
+      class="w-3/4"  
     >
       <div 
-        class="absolute w-full h-full bg-primary/75 backdrop-blur-sm"
-        :style="`clip-path: polygon(${samo ? '0 20%, 0 100%, 80% 100%' : '20% 0, 100% 0, 100% 80%'});`"
+        id="selector" 
+        ref="selector" 
+        class="absolute aspect-video h-dvh max-w-[75dvw] z-10"
       >
-      </div>
-      <div
-        class="absolute w-1/5 h-1/5 bottom-0 right-0 bg-secondary"
-        style="clip-path: polygon(100% 0, 100% 100%, 0 100%);"
-        @click="samo = !samo"
-      >
-        <p
-          class="absolute text-xs md:text-sm bottom-1 sm:bottom-2 right-0.5 sm:right-2 md:right-4 origin-center text-white underline whitespace-nowrap"
+        <div 
+          class="absolute w-full h-full bg-primary/75 backdrop-blur-sm"
+          :style="`clip-path: polygon(${samo ? '0 20%, 0 100%, 80% 100%' : '20% 0, 100% 0, 100% 80%'});`"
         >
-          Click me!
-        </p>
+        </div>
+        <div
+          class="absolute w-1/5 h-1/5 bottom-0 right-0 bg-secondary"
+          style="clip-path: polygon(100% 0, 100% 100%, 0 100%);"
+          @click="samo = !samo"
+        >
+          <p
+            class="absolute text-xs md:text-sm bottom-1 sm:bottom-2 right-0.5 sm:right-2 md:right-4 origin-center text-white underline whitespace-nowrap"
+          >
+            Click me!
+          </p>
+        </div>
       </div>
-    </div>
-    <img
-      :src="`/images/backgrounds/${samo ? 'samo' : 'cind'}.jpg`"
-      class="relative object-cover aspect-video w-full sm:w-3/4"
-      :class="samo ? 'object-center' : 'object-[center_top]'"
-    >
+      <img
+        :src="`/images/backgrounds/${samo ? 'samo' : 'cind'}.jpg`"
+        class="relative object-cover aspect-video h-dvh max-w-[75dvw]"
+        :class="samo ? 'object-center' : 'object-[center_top]'"
+      >
+    </main>
   </div>
 </template>
