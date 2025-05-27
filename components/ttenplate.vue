0707c2f98e505d5839f755beed9c7440f2bd0cf1
Jewel Josef P. Seno <jjpseno@vauldex.com>
2025-05-27 14:41:35 +0800

    Add parallax

--- components/ttenplate.vue
+++ components/ttenplate.vue
@@ -1,10 +1,24 @@
+<script setup lang="ts">
+import { useScroll, useTransform, motion } from "motion-v"
+const { scrollY } = useScroll()
+const objectPosition = ref('0%')
+
+const obPosY = useTransform(scrollY, [0, 500], ['0%', '40%'])
+watchEffect(() => {
+  const unsubscribe = obPosY.on('change', (latest) => {
+    objectPosition.value = latest
+  })
+  return unsubscribe
+})
+</script>
 <template>
   <div
     class="bg-primary"
   >
-    <img 
+    <motion.img 
       src="/images/backgrounds/ttten.svg"
-      class="w-full h-full object-cover object-[50%_0%]"
-    >
-  </div>
+      class="w-full h-full object-cover"
+      :style="{objectPosition: `100% ${objectPosition}` }"
+      />
+    </div>
 </template>
--- pages/plates.vue
+++ pages/plates.vue
@@ -6,15 +6,15 @@
       class="relative h-dvh w-[120%] max-w-screen-2xl mx-auto"
     >
       <Ttenplate
-        class="relative w-full h-[40dvh] top-32 sm:top-54 2xl:top-72 origin-top-left opacity-40"
+        class="relative w-full h-[15dvh] md:h-[40dvh] top-32 sm:top-56 2xl:top-72 origin-top-left opacity-30"
         style="transform: skew(0deg, -6deg);"
       />
       <div
-        class="absolute w-[100%] border-solid border-t-secondary/75 border-t-4 h-[40dvh] top-32 sm:top-54 2xl:top-72 origin-top-left"
+        class="absolute w-[100%] border-solid border-t-secondary/75 border-t-2 h-[15dvh] md:h-[40dvh] top-32 sm:top-56 2xl:top-72 origin-top-left"
         style="transform: skew(0deg, -6deg);"
       />
       <div
-        class="absolute w-[100%] border-solid border-b-secondary/75 border-b-4 h-[40dvh] top-32 sm:top-54 2xl:top-72 origin-top-left"
+        class="absolute w-[100%] border-solid border-b-secondary/75 border-b-2 h-[15dvh] md:h-[40dvh] top-32 sm:top-56 2xl:top-72 origin-top-left"
         style="transform: skew(0deg, -6deg);"
       />
     </section>
